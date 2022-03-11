31c8ae1ae45547ad0b40cbac41ca8012b6f5293c
OlegarioLopez <olelopezlinares@gmail.com>
2022-03-11 07:58:17 +0000

    Update Month.kt

--- app/src/main/java/com/ole/tattoadmin/data/Month.kt
+++ app/src/main/java/com/ole/tattoadmin/data/Month.kt
@@ -1,6 +1,8 @@
 package com.ole.tattoadmin.data
 
 data class Month(
+    var year: Int = 0,
+    var monthNumber: Int=0,
     var monthName: String = "",
     var days: MutableList<Day> = mutableListOf()
 )
