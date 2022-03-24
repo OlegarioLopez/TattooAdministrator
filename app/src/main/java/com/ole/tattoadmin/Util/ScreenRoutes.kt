bf431ea4b345e7c518ae89e12c0b2f68fe47c965
OlegarioLopez <olelopezlinares@gmail.com>
2022-03-24 10:45:27 +0000

    Adding Home Screen

--- app/src/main/java/com/ole/tattoadmin/Util/ScreenRoutes.kt
+++ app/src/main/java/com/ole/tattoadmin/Util/ScreenRoutes.kt
@@ -1,8 +1,10 @@
 package com.ole.tattoadmin.Util
 
 sealed class ScreenRoutes(val route: String){
-    object MainScreen: ScreenRoutes("main_screen")
+    object Home: ScreenRoutes("home")
     object SusccessScreen: ScreenRoutes("susccess_screen ")
     object SpotViewerScreen: ScreenRoutes("spot_viewer_screen")
     object SpotDetailScreen: ScreenRoutes("spot_detail_screen")
+    object SaveMonthData: ScreenRoutes("save_month_data")
+
 }
---
+++ app/src/main/java/com/ole/tattoadmin/presentation/home/Home.kt
@@ -0,0 +1,29 @@
+package com.ole.tattoadmin.presentation.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.ole.tattoadmin.Util.ScreenRoutes
+
+@Composable
+fun Home(navController: NavHostController) {
+
+    Column() {
+        Button(
+            onClick = {
+                navController.navigate(ScreenRoutes.SusccessScreen.route)
+            },
+        ) {
+            Text(text = "Planificar el próximo mes")
+        }
+        Button(
+            onClick = {
+                navController.navigate(ScreenRoutes.SpotViewerScreen.route)
+            },
+        ) {
+            Text(text = "Ver/Modificar citas")
+        }
+    }
+}
