283f4ac73eea84dfbee9a78d4f4ed09e32f83b87
OlegarioLopez <olelopezlinares@gmail.com>
2022-03-26 17:28:06 +0000

    Update SaveMonthDataViewModel.kt

--- app/src/main/java/com/ole/tattoadmin/presentation/saveMonthData/SaveMonthDataViewModel.kt
+++ app/src/main/java/com/ole/tattoadmin/presentation/saveMonthData/SaveMonthDataViewModel.kt
@@ -1,6 +1,8 @@
 package com.ole.tattoadmin.presentation.home
 
 import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -12,12 +14,41 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.TextStyle
+import java.util.*
 
 class saveMonthDataViewModel : ViewModel() {
 
     private val monthsCollectionRef = Firebase.firestore.collection("Months")
     private val daysCollectionRef = Firebase.firestore.collection("Days")
     private val spotsCollectionRef = Firebase.firestore.collection("Spots")
+
+    private var spanishLocale: Locale = Locale("es", "ES")
+    var fmt: DateTimeFormatter = DateTimeFormatterBuilder() // case insensitive
+        .parseCaseInsensitive() // pattern with full month name (MMMM)
+        .toFormatter(Locale("es", "ES"))
+
+    private var _isShiftSelected = mutableStateOf<Boolean>(false)
+    val isShiftSelected: State<Boolean> = _isShiftSelected
+
+    private var _numberShiftSelected = mutableStateOf<Int>(0)
+    val numberShiftSelected: State<Int> = _numberShiftSelected
+
+
+    private var currentDate =
+        LocalDate.now()
+    val listDays =
+        arrayListOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
+    val nextMonth =
+        LocalDate.of(
+            currentDate.year,
+            currentDate.monthValue + 1,
+            currentDate.dayOfMonth
+        ).month.getDisplayName(TextStyle.FULL, spanishLocale)
+//TODO hacer esta variable un estado con remember
 
     fun saveMonth(month: Month) = CoroutineScope(Dispatchers.IO).launch {
         try {
@@ -36,6 +67,10 @@ class saveMonthDataViewModel : ViewModel() {
                 }
             }
         }
+    }
+
+    fun isShiftSelected() {
+        _isShiftSelected.value = !_isShiftSelected.value
     }
 
     fun saveSpots(spots: MutableList<Spot>) = CoroutineScope(Dispatchers.IO).launch {
@@ -75,5 +110,10 @@ class saveMonthDataViewModel : ViewModel() {
                 }
             }
         }
+    }
+
+    fun numberShiftSelected(currentShift: Int) {
+
+        _numberShiftSelected.value = currentShift
     }
 }
